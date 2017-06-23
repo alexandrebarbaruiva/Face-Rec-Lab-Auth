4b4bfd7f5ffc099a5285d2b51d1ddaea20b1f1d0
Caio Calixto Fasolak Alves <caiofasolak@gmail.com>
2017-06-23 14:15:01 -0300

    Adicionado tentativa de função para bd, ainda falta saber como salvar as informações no .json

--- classes.cpp
+++ classes.cpp
@@ -423,3 +423,33 @@ public:
 int Usuario::requisitarAcesso(Laboratorio& laboratorio, int salaDesejada, int horarioDesejado){
 	return laboratorio.requisitarAcesso(*this, salaDesejada, horarioDesejado);
 }
+
+
+/* Exemplo de como criar uma lista ordenada que pode ser utilizada no BD, ainda nao achei como salvar isso no arquivo json...
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+int main()
+{
+    // create JSON values
+    json object = {{"1-CPF", "2-Nome"}};
+
+    // print values
+    std::cout << object << '\n';
+
+    // add values
+    auto res1 = object.emplace("123456789", "Caio");
+    res1 = object.emplace("987654321", "Matheus");
+    res1 = object.emplace("987651234", "Victor");
+    res1 = object.emplace("123459876", "Alexandre");
+
+    auto aluno = object.find("123456789");
+    // print values
+    std::cout << object << '\n';
+    std::cout << *aluno << '\n';
+    std::cout << *res1.first << " " << std::boolalpha << res1.second << '\n';
+}
+;
+}
+*/
